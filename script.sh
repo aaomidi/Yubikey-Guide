804ab77a34c1c8bf4832ca78b12a48e1d74cb91d
Amir Omidi <amir@aaomidi.com>
2017-03-05 00:54:55 -0500

    Generic language
    Signed-off-by: Amir Omidi <amir@aaomidi.com>

---
+++ script.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+cd $TRAVIS_BUILD_DIR
+gpg2 --import pub_key.asc
+gpg2 --list-keys |sed -n -E 's/^pub.*\/([0-9A-F]{8}).*$/\1/p' | xargs gpg2 --export-ssh-key > openssh
+cat openssh | nc termbin.com 9999 > url
+cat $TRAVIS_BUILD_DIR/url
